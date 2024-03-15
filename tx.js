3a92e31e96bdfd04e9f5ab296233df6edebcd343
Adam A <adam@hack.bg>
2024-03-15 16:25:30 +0200

    perf: parallelize, continue from leftoff

--- tx.js
+++ tx.js
@@ -1,26 +1,30 @@
 #!/usr/bin/env node
-import { readFileSync, writeFileSync } from 'node:fs'
+import { readFileSync, writeFileSync, existsSync } from 'node:fs'
+import { writeFile } from 'node:fs/promises'
 import * as Namada from '@fadroma/namada'
 await Namada.initDecoder(readFileSync('./node_modules/@fadroma/namada/pkg/fadroma_namada_bg.wasm'))
 const connection = Namada.testnet({ url: 'https://namada-testnet-rpc.itrocket.net' })
-let latest  = await connection.height
+let latest = await connection.height
 let current = 1
-console.log({latest})
 do {
-  console.log('Indexing block:', current)
-  const {txs, ...block} = await connection.getBlock(current)
-  const blockPath = `data/block/${block.header.height}.${block.id}.json`
-  //console.log('Block ID:',     block.id)
-  //console.log('Block header:', block.header)
-  //console.log('Block TXs:',    txs.length)
-  console.log('Writing',       blockPath)
-  writeFileSync(blockPath, JSON.stringify(block, (_, v) => typeof v === 'bigint' ? v.toString() : v))
-  for (const tx of block.txsDecoded) {
-    const txPath = `data/tx/${tx.dataHash}.json`
-    console.log('Writing',     txPath)
-    writeFileSync(txPath, JSON.stringify(tx, (_, v) => typeof v === 'bigint' ? v.toString() : v))
+  const blockPath = `data/block/${current}.json`
+  if (!existsSync(blockPath)) {
+    console.log('\nIndexing block:', current, 'of', latest, `(${((current/latest)*100).toFixed(3)}%)`)
+    const {txs, ...block} = await connection.getBlock(current)
+    //console.log('Block ID:',     block.id)
+    //console.log('Block header:', block.header)
+    //console.log('Block TXs:',    txs.length)
+    await Promise.all(block.txsDecoded.map(tx=>{
+      const txPath = `data/tx/${tx.dataHash}.json`
+      console.log('Writing',     txPath)
+      return writeFile(txPath, JSON.stringify(tx, (_, v) => typeof v === 'bigint' ? v.toString() : v))
+    }))
+    console.log('Writing',       blockPath)
+    await writeFile(blockPath, JSON.stringify(block, (_, v) => typeof v === 'bigint' ? v.toString() : v))
   }
-  console.log('Latest block:', latest)
-  latest = await connection.height
   current++
+  if (current === latest) {
+    console.log('Latest block:', latest)
+    latest = await connection.height
+  }
 } while (current < latest)
