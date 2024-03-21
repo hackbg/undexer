ecb9f14b62119a76b7b80d492e6cc505af43044d
ivaylonikolov7 <ivnikolov721@gmail.com>
2024-03-21 16:23:07 +0200

    fix: change proposal schema

--- proposal.js
+++ proposal.js
@@ -2,7 +2,7 @@ import init, { Query } from "./shared/pkg/shared.js";
 import { deserialize } from "borsh";
 import { save} from "./utils.js";
 import { mkdirSync, readFileSync } from "node:fs";
-import { ProposalSchema } from "./borsher-schema.js";
+import { ProposalSchema, ProposalsSchema } from "./borsher-schema.js";
 
 await init(readFileSync('shared/pkg/shared_bg.wasm'));
 const q = new Query(process.env.UNDEXER_RPC_URL || 'https://namada-testnet-rpc.itrocket.net');
