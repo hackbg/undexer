00fc6895833921c1ced79866a4661c59d41c5f8d
Milen Radkov <milen@hack.bg>
2024-03-21 21:36:56 +0200

    chore: cleanup proposal generation

--- proposal.js
+++ proposal.js
@@ -3,27 +3,27 @@ import { deserialize } from "borsh";
 import { save } from "./utils.js";
 import { mkdirSync, readFileSync } from "node:fs";
 import { ProposalSchema, ProposalsSchema } from "./borsher-schema.js";
-import 'dotenv/config';
+import "dotenv/config";
 
 await init(readFileSync("shared/pkg/shared_bg.wasm"));
 const q = new Query(
-    process.env.UNDEXER_RPC_URL || "https://namada-testnet-rpc.itrocket.net"
+  process.env.UNDEXER_RPC_URL || "https://namada-testnet-rpc.itrocket.net"
 );
 
 if (process.env.UNDEXER_DATA_DIR) {
-    await mkdirSync(process.env.UNDEXER_DATA_DIR + "/proposals", {
-        recursive: true,
-    });
-    process.chdir(process.env.UNDEXER_DATA_DIR + "/proposals/");
+  await mkdirSync(process.env.UNDEXER_DATA_DIR + "/proposals", {
+    recursive: true,
+  });
+  process.chdir(process.env.UNDEXER_DATA_DIR + "/proposals/");
 } else {
-    throw new Error("set UNDEXER_DATA_DIR");
+  throw new Error("set UNDEXER_DATA_DIR");
 }
 
 const proposals = await q.query_proposals();
 const proposalsDeserialized = deserialize(ProposalsSchema, proposals);
 
 for (let i = 0; i < proposalsDeserialized.length - 1; i++) {
-    const proposalBinary = await q.query_proposal(BigInt(i));
-    const proposalDeserialized = deserialize(ProposalSchema, proposalBinary);
-    await save(`${i}.json`, proposalDeserialized);
+  const proposalBinary = await q.query_proposal(BigInt(i));
+  const proposalDeserialized = deserialize(ProposalSchema, proposalBinary);
+  await save(`${i}.json`, proposalDeserialized);
 }
