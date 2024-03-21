d243cd2b9253c2f23cc5e8468e1ce7de93c26c9f
ivaylonikolov7 <ivnikolov721@gmail.com>
2024-03-21 13:10:28 +0200

    feat: create governance jsons

--- governance.js
+++ governance.js
@@ -1,17 +1,22 @@
-#!/usr/bin/env node
-import { readFileSync } from 'node:fs'
-import * as Namada from '@fadroma/namada'
-await Namada.initDecoder(readFileSync('./node_modules/@fadroma/namada/pkg/fadroma_namada_bg.wasm'))
-const connection = Namada.testnet({ url: 'https://namada-testnet-rpc.itrocket.net' })
-console.log(await connection.getGovernanceParameters())
-const proposals = await connection.getProposalCount()
-console.log('Proposals:', proposals)
-let index = Number(proposals) - 1
-do {
-  console.log(`Fetching proposal ${index}`)
-  const {proposal, votes, result} = await connection.getProposalInfo(index)
-  console.log(`Proposal ${index}:`, proposal)
-  console.log(`Votes on ${index}:`, votes)
-  console.log(`Result of ${index}:`, result)
-  index--
-} while (index >= 0)
+import init, { Query } from "./shared/pkg/shared.js";
+import { deserialize } from "borsh";
+import { mkdirSync, readFileSync } from "node:fs";
+import { save } from "./utils.js";
+import { ProposalsSchema } from "./borsher-schema.js";
+
+await init(readFileSync("shared/pkg/shared_bg.wasm"));
+const q = new Query(
+    process.env.UNDEXER_RPC_URL || "https://namada-testnet-rpc.itrocket.net"
+);
+
+try {
+    await mkdirSync("governance");
+} catch (ex) {
+    console.log("Governance already exists");
+}
+process.chdir("governance");
+
+const proposals = await q.query_proposals();
+
+const propoDeserialized = deserialize(ProposalsSchema, proposals);
+save("proposals.json", propoDeserialized);
