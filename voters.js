157e012eae6116421b623dd077a589dae462148e
Milen Radkov <milen@hack.bg>
2024-03-22 11:01:11 +0200

    feat: collect vote amounts

--- voters.js
+++ voters.js
@@ -1,26 +1,35 @@
 import init, { Query } from "./shared/pkg/shared.js";
-import { deserialize } from "borsh";
 import { save } from "./utils.js";
 import { mkdirSync, readFileSync } from "node:fs";
-import { ProposalSchema, ProposalsSchema } from "./borsher-schema.js";
+import "dotenv/config";
+import fs from "fs";
 
 await init(readFileSync("shared/pkg/shared_bg.wasm"));
 const q = new Query(
-    process.env.UNDEXER_RPC_URL || "http://namada-testnet-rpc.stakeandrelax.net"
+  process.env.UNDEXER_RPC_URL || "http://namada-testnet-rpc.stakeandrelax.net"
 );
 
 if (process.env.UNDEXER_DATA_DIR) {
-    await mkdirSync(process.env.UNDEXER_DATA_DIR + "/voters", {
-        recursive: true,
-    });
-    process.chdir(process.env.UNDEXER_DATA_DIR + "/voters/");
+  await mkdirSync(process.env.UNDEXER_DATA_DIR + "/voters", {
+    recursive: true,
+  });
+  process.chdir(process.env.UNDEXER_DATA_DIR + "/voters/");
 } else {
-    throw new Error("set UNDEXER_DATA_DIR");
+  throw new Error("set UNDEXER_DATA_DIR");
 }
 
-const lastProposal = await q.last_proposal_id();
+const allProposals = JSON.parse(
+  fs.readFileSync(`../proposals/all_proposals.json`, "utf8")
+);
 
-for(let i = 1; i < Number(lastProposal); i++) {
-    const voters = await q.get_voters_power_by_proposal_id(BigInt(1), BigInt(12));
-    await save(`${i}.json`, voters);
+for (let i = 0; i <= allProposals.lenght; i++) {
+  const proposal = JSON.parse(
+    fs.readFileSync(`../proposals/${i}.json`, "utf8")
+  );
+  console.log(`${i}/${allProposals.length}`);
+  const voters = await q.query_voters_power_by_proposal_id(
+    BigInt(i),
+    BigInt(Number(proposal.endEpoch))
+  );
+  await save(`${i}.json`, voters);
 }
