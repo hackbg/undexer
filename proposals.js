10061982addf2ca69468cd1f973144cb1b19abbc
Milen Radkov <milen@hack.bg>
2024-03-22 10:59:55 +0200

    feat: add dotenv to proposals

--- proposals.js
+++ proposals.js
@@ -3,27 +3,20 @@ import { deserialize } from "borsh";
 import { mkdirSync, readFileSync } from "node:fs";
 import { makeDirIfItDoesntExist, save } from "./utils.js";
 import { ProposalsSchema } from "./borsher-schema.js";
+import "dotenv/config";
 
 await init(readFileSync("shared/pkg/shared_bg.wasm"));
 const q = new Query(
-    process.env.UNDEXER_RPC_URL || "https://namada-testnet-rpc.itrocket.net"
+  process.env.UNDEXER_RPC_URL || "https://namada-testnet-rpc.itrocket.net"
 );
 
-
-
-try {
-    
-} catch (ex) {
-    console.log("Governance already exists");
-}
-
-if(process.env.UNDEXER_DATA_DIR){
-    await mkdirSync(process.env.UNDEXER_DATA_DIR+'/proposals', {recursive: true});
-    process.chdir(process.env.UNDEXER_DATA_DIR+'/proposals/');
-    
-}
-else{
-    throw new Error('set UNDEXER_DATA_DIR');
+if (process.env.UNDEXER_DATA_DIR) {
+  await mkdirSync(process.env.UNDEXER_DATA_DIR + "/proposals", {
+    recursive: true,
+  });
+  process.chdir(process.env.UNDEXER_DATA_DIR + "/proposals/");
+} else {
+  throw new Error("set UNDEXER_DATA_DIR");
 }
 
 const proposals = await q.query_proposals();
