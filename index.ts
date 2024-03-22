0d67dc69f3edfa65c833cadb00873b19e14a10f5
Adam A <adam@hack.bg>
2024-03-22 19:02:45 +0200

    chore: enable typescript

---
+++ index.ts
@@ -0,0 +1,5 @@
+import Commands from '@hackbg/cmds'
+export default class UndexerCommands extends Commands {
+  // see https://github.com/hackbg/fadroma/blob/v2/packages/namada/namada.ts
+  // for examples how to define commands
+}
