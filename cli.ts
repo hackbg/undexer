66112f541553f7c3948e74a5347e127472330a7d
Adam A <adam@hack.bg>
2024-04-09 17:36:41 +0300

    feat: command to decode block; fix decrypted tx id

--- cli.ts
+++ cli.ts
@@ -1,9 +1,17 @@
 import * as Namada from '@fadroma/namada';
 import Commands from "@hackbg/cmds";
+import { pathToFileURL, fileURLToPath } from 'node:url';
+import { resolve, dirname } from 'node:path';
+import { readFileSync } from 'node:fs';
 
 export default class UndexerCommands extends Commands {
   // see https://github.com/hackbg/fadroma/blob/v2/packages/namada/namada.ts
   // for examples how to define commands
+
+  constructor (...args) {
+    super(...args)
+    this.log.label = 'Undexer'
+  }
 
   txs = this.command({
     name: "txs",
@@ -38,27 +46,37 @@ export default class UndexerCommands extends Commands {
     args: 'BLOCK_HEIGHT [RPC_URL]'
   }, async (height: number, url: string) => {
     height = Number(height)
-    const connection = Namada.testnet()
+    url ||= Namada.testnet().url
+    const decoder = readFileSync(resolve(
+      dirname(fileURLToPath(import.meta.url)),
+      'node_modules/@fadroma/namada/pkg/fadroma_namada_bg.wasm',
+    ))
+    const connection = await Namada.connect({ url, decoder })
     const [
       blockResponse,
       blockResultsResponse
     ] = await Promise.all([
       fetch(`${connection.url}/block?height=${height}`)
-        .then(response=>response.json()),
+        .then(response=>response.text()),
       fetch(`${connection.url}/block_results?height=${height}`)
-        .then(response=>response.json()),
+        .then(response=>response.text()),
     ])
-    console.log({
-      url,
-      height,
-      connection,
-      blockResponse,
-      blockResultsResponse
-    })
-    connection.decode.decoded_block(
+    //this.log({
+      //url,
+      //height,
+      //connection,
+      //blockResponse,
+      //blockResultsResponse
+    //})
+    const decoded = connection.decode.block(
       blockResponse,
       blockResultsResponse
     )
+    for (const tx of decoded.txs) {
+      //if (tx.content.type !== 'tx_init_proposal.wasm') continue
+      this.log(tx)
+      //this.log(tx.content)
+    }
   })
 
 }
