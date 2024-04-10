5fae90b38f9357891da486bd53bdbe0fd4c48f9f
Milen Radkov <milenradkov@me.com>
2024-04-10 08:51:22 +0300

    feat(api): search block by height or hash

--- api/index.js
+++ api/index.js
@@ -1,69 +1,74 @@
-import express from "express";
-import sequelizer from "../db/index.js";
-import Block from "../models/Block.js";
-import Transaction from "../models/Transaction.js";
-import Validator from "../models/Validator.js";
+import express from 'express';
+import sequelizer from '../db/index.js';
+import Block from '../models/Block.js';
+import Transaction from '../models/Transaction.js';
+import Validator from '../models/Validator.js';
 
 const app = express();
 
-app.get("/block/:height", async (req, res) => {
+app.get('/block/:height', async (req, res) => {
   const block = await Block.findOne(
     {
-      offset: req.params.height - 1,
+      where: {
+        height: req.params.height,
+      },
       include: Transaction,
     },
     {
       raw: true,
-    }
+    },
   );
   if (block === null) {
-    res.status(404).send({ error: "Block not found" });
+    res.status(404).send({ error: 'Block not found' });
     return;
   }
   res.status(200).send(block);
 });
 
-app.get("/block/hash/:hash", async (req, res) => {
+app.get('/block/hash/:hash', async (req, res) => {
   const block = await Block.findOne(
     {
-      txHash: req.params.hash,
+      where: {
+        id: req.params.hash,
+      },
+      include: Transaction,
     },
     {
       raw: true,
-    }
+    },
   );
   if (block === null) {
-    res.status(404).send({ error: "Block not found" });
+    res.status(404).send({ error: 'Block not found' });
     return;
   }
 
   res.status(200).send(block);
 });
 
-app.get("/tx/:txHash", async (req, res) => {
-  res.status(501).send({ error: "Not implemented" });
+app.get('/tx/:txHash', async (req, res) => {
+  res.status(501).send({ error: 'Not implemented' });
   /*
   const tx = await Transaction.findOne({ id: req.params.txHash, raw: true });
   res.status(200).send(tx);
   */
 });
 
-app.get("/validators", (req, res) => {
+app.get('/validators', (req, res) => {
   const validators = Validator.findAll({ raw: true });
   res.status(200).send(validators);
 });
 
-app.get("/validator/:type", (req, res) => {
+app.get('/validator/:type', (req, res) => {
   const validator = Validator.findAll(
     {
       where: {
         type: req.params.type,
       },
     },
-    { raw: true }
+    { raw: true },
   );
   if (validator === null) {
-    res.status(404).send({ error: "Validator not found" });
+    res.status(404).send({ error: 'Validator not found' });
     return;
   }
 
