21144253801c91af3727a9ae84fcd9dd2a55261f
Milen Radkov <milenradkov@me.com>
2024-04-10 08:51:49 +0300

    feat(models): block: add height, associate transaction with block

--- models/Block.js
+++ models/Block.js
@@ -1,30 +1,40 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 import { DataTypes } from 'sequelize';
-import sequelizer from "../db/index.js";
+import sequelize from '../db/index.js';
 import Transaction from './Transaction.js';
 
-const Block = sequelizer.define('block', {
-    id: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        unique: true,
-        primaryKey: true,
-    },
-    header: {
-        type: DataTypes.JSON,
-        allowNull: false,
-    },
+const Block = sequelize.define('block', {
+  id: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    unique: true,
+    primaryKey: true,
+  },
+  header: {
+    type: DataTypes.JSON,
+    allowNull: false,
+  },
+  height: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
 });
 
 Block.hasMany(Transaction);
+Transaction.hasOne(Block);
 
 export default Block;
 
-export async function getLatestBlockInDB () {
-  return (await Block.findAll({
-    raw: true,
-    attributes: [
-      Sequelize.fn('max', Sequelize.cast(Sequelize.json('header.height'), 'int'))
-    ]
-  }))[0].max
+export async function getLatestBlockInDB() {
+  return (
+    await Block.findAll({
+      raw: true,
+      attributes: [
+        Sequelize.fn(
+          'max',
+          Sequelize.cast(Sequelize.json('header.height'), 'int'),
+        ),
+      ],
+    })
+  )[0].max;
 }
