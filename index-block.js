b31dc2135d70e7c2abcb4916b8ac92ad8485325d
Milen Radkov <milenradkov@me.com>
2024-04-10 10:05:51 +0300

    fix: block is stored in db first and then txs
    If both db writes are in the same promise, psql throws a constraint validation error as the blockId foreignKey is not yet present in db to make the relation.

--- index-block.js
+++ index-block.js
@@ -1,96 +1,100 @@
-import * as Namada from '@fadroma/namada'
-import getRPC from './connection.js'
-import Block from "./models/Block.js";
-import Transaction from "./models/Transaction.js";
+import * as Namada from '@fadroma/namada';
+import getRPC from './connection.js';
+import Block from './models/Block.js';
+import Transaction from './models/Transaction.js';
 import { WASM_TO_CONTENT } from './models/Contents/index.js';
 import { NAME_TO_SECTION } from './models/Sections/index.js';
-import { format } from "./utils.js";
+import { format } from './utils.js';
 
-const console = new Namada.Core.Console('Block')
+const console = new Namada.Core.Console('Block');
 
-const fetchText = url => {
-  console.log(`Fetching ${url}`)
-  return fetch(url).then(response=>response.text())
-}
+const fetchText = (url) => {
+  console.log(`Fetching ${url}`);
+  return fetch(url).then((response) => response.text());
+};
 
-export default async function indexBlock (height, events) {
-
-  const t0 = performance.now()
+export default async function indexBlock(height, events) {
+  const t0 = performance.now();
 
   // Get connection correspondinf to block height
-  const { connection, query } = getRPC(height)
+  const { connection, query } = getRPC(height);
 
   // Fetch block data
-  console.debug('Fetching block', height)
-  const [ block, blockResponse, blockResultsResponse ] = await Promise.all([
+  console.debug('Fetching block', height);
+  const [block, blockResponse, blockResultsResponse] = await Promise.all([
     connection.getBlock(height),
     fetchText(`${connection.url}/block?height=${height}`),
     fetchText(`${connection.url}/block_results?height=${height}`),
-  ])
+  ]);
 
   // Decode block data
-  console.debug('Decoding block', height)
-  const { id, txs } = connection.decode.block(blockResponse, blockResultsResponse)
+  console.debug('Decoding block', height);
+  const { id, txs } = connection.decode.block(
+    blockResponse,
+    blockResultsResponse,
+  );
 
   // Write block and all transactions to database.
-  console.debug('Storing block', height, block.id)
+  console.debug('Storing block', height, block.id);
 
   // TODO: Wrap this big promise in a PostgreSQL transaction
   //       so that block/transactions/sections/contents are either
   //       saved fully, or not at all!
-  await Promise.all([
 
+  await Promise.all([
     // Block
     Block.create({
       height,
-      ...block
+      ...block,
     }),
+  ]);
 
+  await Promise.all([
     // Each transaction in the block:
-    ...txs.map(tx=>Promise.all([
+    ...txs.map((tx) =>
+      Promise.all([
+        // The transaction
+        Transaction.create({
+          ...tx,
+          id: undefined,
+          txId: tx.id,
+          blockId: block.id,
+        }),
 
-      // The transaction
-      Transaction.create({
-        ...tx,
-        id:   undefined,
-        txId: tx.id,
-        blockId: block.id
-      }),
+        // The transaction's content
+        WASM_TO_CONTENT[tx.content.type].create(
+          format(Object.assign(tx.content)).data,
+        ),
 
-      // The transaction's content
-      WASM_TO_CONTENT[tx.content.type].create(
-        format(Object.assign(tx.content)).data
-      ),
-
-      // The transaction's sections
-      // TODO: MaspTx section is not implemented in schema
-      ...tx.sections.map(section=>{
-        const Section = NAME_TO_SECTION[section.type]
-        if (!Section) {
-          console.warn('Unknown section type', section.type)
-          return false
-        }
-        return Section.create(section)
-      }).filter(Boolean)
-
-    ]))
-
-  ])
+        // The transaction's sections
+        // TODO: MaspTx section is not implemented in schema
+        ...tx.sections
+          .map((section) => {
+            const Section = NAME_TO_SECTION[section.type];
+            if (!Section) {
+              console.warn('Unknown section type', section.type);
+              return false;
+            }
+            return Section.create(section);
+          })
+          .filter(Boolean),
+      ]),
+    ),
+  ]);
 
   // Write block and all transactions to database.
-  console.debug('Indexed in', performance.now() - t0, 'msec')
+  console.debug('Indexed in', performance.now() - t0, 'msec');
 
   // If the event emitter has been passed in,
   // emit events for transactions of interest
   if (events) {
     for (const tx of txs) {
       if (tx.content.type === 'tx_init_proposal.wasm') {
-        events.emit('proposal', { height })
+        events.emit('proposal', { height });
       }
       if (tx.content.type === 'tx_vote_proposal.wasm') {
-        events.emit('vote', { height, proposal: tx.content.data.proposalId })
+        events.emit('vote', { height, proposal: tx.content.data.proposalId });
       }
     }
   }
-
 }
