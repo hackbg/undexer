1c42268bd985d6f936db6e7486b347d04fc6ba32
Milen Radkov <milenradkov@me.com>
2024-04-10 08:52:34 +0300

    chore(models): transaction

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,33 +1,33 @@
 import { DataTypes } from "sequelize";
-import sequelizer from "../db/index.js";
+import sequelize from "../db/index.js";
 import Section from "./Section.js";
 import Content from "./Content.js";
+// import Block from "./Block.js";
 
-const Transaction = sequelizer.define('transaction', {
-    txId: {
-        type: DataTypes.TEXT,
-    },
-    chainId: {
-        type: DataTypes.TEXT,
-    },
-    expiration: {
-        type: DataTypes.DATE,
-    },
-    timestamp: {
-        type: DataTypes.DATE,
-    },
-    codeHash: {
-        type: DataTypes.TEXT,
-    },
-    dataHash: {
-        type: DataTypes.TEXT,
-    },
-    memoHash: {
-        type: DataTypes.TEXT,
-    },
+const Transaction = sequelize.define('transaction', {
+  txId: {
+    type: DataTypes.TEXT,
+  },
+  chainId: {
+    type: DataTypes.TEXT,
+  },
+  expiration: {
+    type: DataTypes.DATE,
+  },
+  timestamp: {
+    type: DataTypes.DATE,
+  },
+  codeHash: {
+    type: DataTypes.TEXT,
+  },
+  dataHash: {
+    type: DataTypes.TEXT,
+  },
+  memoHash: {
+    type: DataTypes.TEXT,
+  },
 });
 
-Transaction.hasOne(Section);
 Transaction.hasOne(Content);
 
 export default Transaction;
