696c7a619a6da8c728be386eadbef13226e990ce
Milen Radkov <milenradkov@me.com>
2024-04-11 17:47:43 +0300

    feat(api): validators uptime by address

--- api/index.js
+++ api/index.js
@@ -13,33 +13,35 @@ const app = express();
 const router = express.Router();
 
 // CORS-enabled for all origins
-app.use(cors())
-app.use('/v2', router)
+app.use(cors());
+app.use('/v2', router);
 
 router.get('/block/latest', async (req, res) => {
-  const latestBlock = await Block.max('height')
-  res.status(200).send(latestBlock.toString())
-})
+  const latestBlock = await Block.max('height');
+  res.status(200).send(latestBlock.toString());
+});
 
 router.get('/blocks', async (req, res) => {
-  const limit = req.query.limit ? Number(req.query.limit) : 20
-  const offset = req.query.offset ? Number(req.query.offset) : 0
+  const limit = req.query.limit ? Number(req.query.limit) : 20;
+  const offset = req.query.offset ? Number(req.query.offset) : 0;
   const { rows, count } = await Block.findAndCountAll({
     order: [['height', 'DESC']],
     limit,
     offset,
     attributes: ['height', 'id', 'header'],
-  })
-  const blocks = await Promise.all(rows.map(async (block) => {
-    const txs = await Transaction.findAll({
-      where: { blockHeight: block.height },
-      attributes: ['txId']
-    })
-    return { ...block.get(), txs }
-  }))
+  });
+  const blocks = await Promise.all(
+    rows.map(async (block) => {
+      const txs = await Transaction.findAll({
+        where: { blockHeight: block.height },
+        attributes: ['txId'],
+      });
+      return { ...block.get(), txs };
+    }),
+  );
 
-  res.status(200).send({ count, blocks })
-})
+  res.status(200).send({ count, blocks });
+});
 
 router.get('/block/:height', async (req, res) => {
   const block = await Block.findOne(
@@ -107,12 +109,12 @@ router.get('/tx/:txHash', async (req, res) => {
   res.status(200).send(tx);
 });
 
-router.get('/validators', (req, res) => {
+router.get('/validators', async (req, res) => {
   const validators = Validator.findAll({ raw: true });
   res.status(200).send(validators);
 });
 
-router.get('/validator/:type', (req, res) => {
+router.get('/validator/:type', async (req, res) => {
   const validator = Validator.findAll(
     {
       where: {
@@ -129,10 +131,36 @@ router.get('/validator/:type', (req, res) => {
   res.status(200).send(validator);
 });
 
-router.get('/proposals/', (req, res) => {
+router.get('/validator/uptime/:address', async (req, res) => {
+  const address = req.params.address;
+  const blocks = await Block.findAll(
+    {
+      order: [['height', 'DESC']],
+      limit: 100,
+      attributes: ['rpcResponse', 'height'],
+    },
+    { raw: true },
+  );
+
+  const currentHeight = blocks[0].height;
+  const countedBlocks = blocks.length;
+
+  const uptime = blocks
+    .map((b) => {
+      return JSON.parse(b.rpcResponse).result.block.last_commit.signatures.map(
+        (x) => x.validator_address,
+      );
+    })
+    .flat(1)
+    .filter((x) => x == address).length;
+
+  res.status(200).send({ uptime, currentHeight, countedBlocks });
+});
+
+router.get('/proposals/', async (req, res) => {
   const proposals = Proposal.findAll({ raw: true });
   res.status(200).send(proposals);
-})
+});
 
 router.get('/proposal/:id', (req, res) => {
   const proposal = Proposal.findOne(
@@ -149,7 +177,7 @@ router.get('/proposal/:id', (req, res) => {
   }
 
   res.status(200).send(proposal);
-})
+});
 
 const { SERVER_PORT: port = 8888 } = process.env;
 
