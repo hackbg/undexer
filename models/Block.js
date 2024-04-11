0dd3d2525d0e1549a382feb8cf446b0dd5ee9f2d
Milen Radkov <milenradkov@me.com>
2024-04-11 17:47:43 +0300

    feat(api): validators uptime by address

--- models/Block.js
+++ models/Block.js
@@ -17,15 +17,33 @@ const Block = sequelize.define('block', {
   header: {
     type: DataTypes.JSON,
     allowNull: false,
+    get() {
+      return JSON.parse(this.getDataValue('header'));
+    },
+    set(value) {
+      return this.setDataValue('header', JSON.stringify(value));
+    },
   },
   results: {
-    type: DataTypes.JSONB,
-    allowNull: false
+    type: DataTypes.JSON,
+    allowNull: false,
+    get() {
+      return JSON.parse(this.getDataValue('results'));
+    },
+    set(value) {
+      return this.setDataValue('results', JSON.stringify(value));
+    },
   },
   rpcResponse: {
-    type: DataTypes.JSONB,
-    allowNull: false
-  }
+    type: DataTypes.JSON,
+    allowNull: false,
+    get() {
+      return JSON.parse(this.getDataValue('rpcResponse'));
+    },
+    set(value) {
+      return this.setDataValue('rpcResponse', JSON.stringify(value));
+    },
+  },
 });
 
 Block.hasMany(Transaction);
