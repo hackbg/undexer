0869428f4f385aaeec1888c57e5344f09ad7ee4b
Adam A <adam@hack.bg>
2024-06-06 13:35:55 +0300

    fix: fetch block command

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-import * as Namada from '@fadroma/namada';
-import Commands from "@hackbg/cmds";
-import { pathToFileURL, fileURLToPath } from 'node:url';
-import { resolve, dirname } from 'node:path';
-import { readFileSync } from 'node:fs';
+import * as Namada from '@fadroma/namada'
+import Commands from "@hackbg/cmds"
+import { pathToFileURL, fileURLToPath } from 'node:url'
+import { resolve, dirname } from 'node:path'
+import { readFileSync } from 'node:fs'
+import getRPC from './src/config/connection.js'
 
 export default class UndexerCommands extends Commands {
   // see https://github.com/hackbg/fadroma/blob/v2/packages/namada/namada.ts
@@ -26,40 +27,11 @@ export default class UndexerCommands extends Commands {
   block = this.command({
     name: 'block',
     info: 'fetch and decode a block',
-    args: 'BLOCK_HEIGHT [RPC_URL]'
-  }, async (height: number, url: string) => {
-    height = Number(height)
-    url ||= Namada.testnet().url
-    const decoder = readFileSync(resolve(
-      dirname(fileURLToPath(import.meta.url)),
-      'node_modules/@fadroma/namada/pkg/fadroma_namada_bg.wasm',
-    ))
-    const connection = await Namada.connect({ url, decoder })
-    const [
-      blockResponse,
-      blockResultsResponse
-    ] = await Promise.all([
-      fetch(`${connection.url}/block?height=${height}`)
-        .then(response=>response.text()),
-      fetch(`${connection.url}/block_results?height=${height}`)
-        .then(response=>response.text()),
-    ])
-    //this.log({
-      //url,
-      //height,
-      //connection,
-      //blockResponse,
-      //blockResultsResponse
-    //})
-    const decoded = connection.decode.block(
-      blockResponse,
-      blockResultsResponse
-    )
-    for (const tx of decoded.txs) {
-      //if (tx.content.type !== 'tx_init_proposal.wasm') continue
-      this.log(tx)
-      //this.log(tx.content)
-    }
+    args: 'HEIGHT'
+  }, async (height: number) => {
+    const { connection } = await getRPC(height)
+    const block = await connection.fetchBlock({ height })
+    console.log(block)
   })
 
 }
