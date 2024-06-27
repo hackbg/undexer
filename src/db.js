05ca175700dcd5101a84e38107925c238f41ce99
Adam A <adam@hack.bg>
2024-06-28 00:44:28 +0300

    refactor(db): define all models in 1 exporession

--- src/db.js
+++ src/db.js
@@ -1,11 +1,13 @@
 import { Console } from "@hackbg/logs"
+import { Sequelize, DataTypes, Op } from "sequelize"
+import { DATABASE_URL } from "./config.js"
+
+export { Sequelize, DataTypes, Op }
+
 const console = new Console("DB");
 
-import { Sequelize, DataTypes, Op } from "sequelize"
-export { Sequelize, DataTypes, Op }
 const { DATE, TEXT, JSONB, INTEGER, ENUM } = DataTypes
 
-import { DATABASE_URL } from "./config.js"
 const db = new Sequelize(DATABASE_URL, {
   dialect: "postgres",
   logging: () => console.log,
@@ -22,33 +24,6 @@ export const IntegerPrimaryKey = (autoIncrement = false) => ({
   primaryKey: true,
   autoIncrement
 })
-
-export const ErrorLog = db.define('error_log', {
-  id:        IntegerPrimaryKey(true),
-  timestamp: { type: DATE },
-  message:   { type: TEXT },
-  stack:     { type: JSONB },
-  info:      { type: JSONB, allowNull: true },
-})
-
-export function logErrorToDB (error, info) {
-  return ErrorLog.create({
-    timestamp: new Date(),
-    message:   error?.message,
-    stack:     error?.stack,
-    info
-  })
-}
-
-export async function withErrorLog (callback, info) {
-  try {
-    return Promise.resolve(callback())
-  } catch (error) {
-    console.error('Logging error to database:', error)
-    await logErrorToDB(error, info)
-    throw error
-  }
-}
 
 export const StringPrimaryKey = () => ({
   type:       TEXT,
@@ -83,38 +58,11 @@ export const VALIDATOR_STATES = [
   "Inactive"
 ]
 
-export const Validator = db.define('validator', {
-  publicKey:        StringPrimaryKey(),
-  address:          { type: TEXT, allowNull: true },
-  namadaAddress:    { type: TEXT, allowNull: true },
-  votingPower:      { type: TEXT, allowNull: true },
-  proposerPriority: { type: TEXT, allowNull: true },
-  metadata:         NullableJSONField('metadata'),
-  commission:       NullableJSONField('commission'),
-  stake:            { type: TEXT, allowNull: true },
-  state:            NullableJSONField('state')
-})
-
-const blockMeta = {
+const blockMeta = () => ({
   chainId:      { type: TEXT,    allowNull: false },
   blockHash:    { type: TEXT,    allowNull: false },
   blockHeight:  { type: INTEGER, allowNull: false },
   blockTime:    { type: DATE },
-}
-
-export const Block = db.define('block', {
-  ...blockMeta,
-  blockHash:    StringPrimaryKey(),
-  blockHeader:  JSONField('blockHeader'),
-  //blockResults: JSONField('blockResults'), // TODO deserialize results response
-  rpcResponses: JSONField('rpcResponses'),
-})
-
-export const Transaction = db.define('transaction', {
-  ...blockMeta,
-  txHash: StringPrimaryKey(),
-  txTime: { type: DATE },
-  txData: JSONField('txData'),
 })
 
 export const PROPOSAL_STATUS = [
@@ -134,15 +82,71 @@ export const PROPOSAL_TALLY_TYPE = [
   "LessOneHalfOverOneThirdNay"
 ]
 
-export const Proposal = db.define('proposal', {
-  id:       IntegerPrimaryKey(),
-  content:  JSONField('content'),
-  metadata: JSONField('metadata'),
-  result:   NullableJSONField('result'),
-})
+export const
 
-export const Vote = db.define("vote", {
-  id:         IntegerPrimaryKey(true),
-  proposal:   { type: INTEGER },
-  data:       JSONField('data'),
-})
+  ErrorLog = db.define('error_log', {
+    id:        IntegerPrimaryKey(true),
+    timestamp: { type: DATE },
+    message:   { type: TEXT },
+    stack:     { type: JSONB },
+    info:      { type: JSONB, allowNull: true },
+  }),
+
+  Validator = db.define('validator', {
+    publicKey:        StringPrimaryKey(),
+    address:          { type: TEXT, allowNull: true },
+    namadaAddress:    { type: TEXT, allowNull: true },
+    votingPower:      { type: TEXT, allowNull: true },
+    proposerPriority: { type: TEXT, allowNull: true },
+    metadata:         NullableJSONField('metadata'),
+    commission:       NullableJSONField('commission'),
+    stake:            { type: TEXT, allowNull: true },
+    state:            NullableJSONField('state')
+  }),
+
+  Block = db.define('block', {
+    ...blockMeta(),
+    blockHash:    StringPrimaryKey(),
+    blockHeader:  JSONField('blockHeader'),
+    //blockResults: JSONField('blockResults'), // TODO deserialize results response
+    rpcResponses: JSONField('rpcResponses'),
+  }),
+
+  Transaction = db.define('transaction', {
+    ...blockMeta(),
+    txHash: StringPrimaryKey(),
+    txTime: { type: DATE },
+    txData: JSONField('txData'),
+  }),
+
+  Proposal = db.define('proposal', {
+    id:       IntegerPrimaryKey(),
+    content:  JSONField('content'),
+    metadata: JSONField('metadata'),
+    result:   NullableJSONField('result'),
+  }),
+
+  Vote = db.define("vote", {
+    id:       IntegerPrimaryKey(true),
+    proposal: { type: INTEGER },
+    data:     JSONField('data'),
+  });
+
+export function logErrorToDB (error, info) {
+  return ErrorLog.create({
+    timestamp: new Date(),
+    message:   error?.message,
+    stack:     error?.stack,
+    info
+  })
+}
+
+export async function withErrorLog (callback, info) {
+  try {
+    return Promise.resolve(callback())
+  } catch (error) {
+    console.error('Logging error to database:', error)
+    await logErrorToDB(error, info)
+    throw error
+  }
+}
